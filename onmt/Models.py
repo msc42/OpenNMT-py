126aea8554ff4f6d4a6a8bd7e255ccddec83ea31
Quan Pham <ngoc.pham@kit.edu>
2017-07-05 19:02:12 +0200

    Sampling and GLEU score completed

--- onmt/Models.py
+++ onmt/Models.py
@@ -4,6 +4,8 @@ from torch.autograd import Variable
 import onmt.modules
 from torch.nn.utils.rnn import pad_packed_sequence as unpack
 from torch.nn.utils.rnn import pack_padded_sequence as pack
+import random
+
 
 
 class Encoder(nn.Module):
@@ -14,6 +16,7 @@ class Encoder(nn.Module):
         assert opt.rnn_size % self.num_directions == 0
         self.hidden_size = opt.rnn_size // self.num_directions
         input_size = opt.word_vec_size
+        dropout_value = opt.dropout 
 
         super(Encoder, self).__init__()
         self.word_lut = nn.Embedding(dicts.size(),
@@ -21,7 +24,7 @@ class Encoder(nn.Module):
                                      padding_idx=onmt.Constants.PAD)
         self.rnn = nn.LSTM(input_size, self.hidden_size,
                            num_layers=opt.layers,
-                           dropout=opt.dropout,
+                           dropout=dropout_value,
                            bidirectional=opt.brnn)
 
     def load_pretrained_vectors(self, opt):
@@ -56,7 +59,7 @@ class StackedLSTM(nn.Module):
     def forward(self, input, hidden):
         h_0, c_0 = hidden
         h_1, c_1 = [], []
-        for i, layer in enumerate(self.layers):
+        for i, layer in enumerate(self.layers):	
             h_1_i, c_1_i = layer(input, (h_0[i], c_0[i]))
             input = h_1_i
             if i + 1 != self.num_layers:
@@ -89,6 +92,7 @@ class Decoder(nn.Module):
         self.dropout = nn.Dropout(opt.dropout)
 
         self.hidden_size = opt.rnn_size
+        self.input_size = input_size
 
     def load_pretrained_vectors(self, opt):
         if opt.pre_word_vecs_dec is not None:
@@ -119,10 +123,23 @@ class Decoder(nn.Module):
 
 class NMTModel(nn.Module):
 
-    def __init__(self, encoder, decoder):
+    def __init__(self, encoder, decoder, generator, criterion):
         super(NMTModel, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
+        self.generator = generator
+        self.criterion = criterion
+    
+    # For sampling functions, we need to create an initial input
+    # Which is vector of batch_size full of BOS    
+    def make_init_input(self, src):
+				if isinstance(src, tuple):
+					src = src[0]
+				batch_size = src.size(1)
+				i_size = (1, batch_size)
+				
+				input_vector = src.data.new(*i_size).fill_(onmt.Constants.BOS)
+				return Variable(input_vector, requires_grad=False)
 
     def make_init_decoder_output(self, context):
         batch_size = context.size(1)
@@ -138,17 +155,152 @@ class NMTModel(nn.Module):
                     .view(h.size(0) // 2, h.size(1), h.size(2) * 2)
         else:
             return h
+            
+    def sample(self, input, max_length=100, argmax=True):
+			
+				
+				src = input[0]
+				# we don't care about tgt here
+				
+				enc_hidden, context = self.encoder(src)
+				
+				enc_hidden = (self._fix_enc_hidden(enc_hidden[0]),
+                      self._fix_enc_hidden(enc_hidden[1]))
+				
+				sampled = []
+				
+				# we start from the vector of <BOS>				
+				input_t = self.make_init_input(src)
+				
+				# For input feeding initial output
+				init_output = self.make_init_decoder_output(context)
+				
+				hidden = enc_hidden
+				state = init_output
+				
+				tensor_check = None
+				batch_size = context.size(1)
+				
+				lengths = torch.Tensor(batch_size).fill_(max_length).type_as(input_t.data)
+				
+				for t in xrange(max_length):
+					# make a forward pass through the decoder
+					state, hidden, attn_t = self.decoder(input_t, hidden, context, state)
+					
+					# from the state, compute the probability distribution
+					# squeeze because state has size 1 x BS x H (due to concat)
+					state = state.squeeze(0)
+					output = self.generator(state) 
+					
+					if argmax:
+						topv, topi = output.data.topk(1)
+						
+						input_t = Variable(topi.t())
+					else: # Stochastic sampling
+						input_t = output.exp().multinomial().t()
 
-    def forward(self, input):
+					# condition to stop the sampling procedure
+					check = input_t.eq(onmt.Constants.EOS)
+					if tensor_check is None:
+						tensor_check  = check
+					else:
+						tensor_check += check
+					
+					
+					lengths.masked_fill_(check.squeeze(0).data, t+1)
+					
+					# Every sampled input after EOS is just PAD
+					input_t.masked_fill(tensor_check, onmt.Constants.PAD)
+
+					sampled.append(input_t)
+					
+					# if all sentences are finished (reach EOS)
+					#~ print(tensor_check.sum().data[0])
+					if tensor_check.sum().data[0] == batch_size:
+						break
+						
+				lengths = [lengths[i] for i in xrange(batch_size)]
+					
+				return sampled, lengths
+
+    def forward(self, input, eval=False, mode='xe', teacher_forcing_ratio = 1):
         src = input[0]
         tgt = input[1][:-1]  # exclude last target from inputs
+        # Exclude <s> from targets for labels
+        tgt_label = input[1][1:]
+        
         enc_hidden, context = self.encoder(src)
         init_output = self.make_init_decoder_output(context)
 
         enc_hidden = (self._fix_enc_hidden(enc_hidden[0]),
                       self._fix_enc_hidden(enc_hidden[1]))
+                      
+        states = []
+        
+        outputs = []
+        
+        hidden = enc_hidden
+        state = init_output
+        batch_size = tgt.size(1) 
+        length = tgt.size(0)
+        
+        
+        # Here we loop over the input
+        for t, input_t in enumerate(tgt.split(1)):
+					
+					use_teacher_forcing = random.random() <= teacher_forcing_ratio
+					
+					
+					if not use_teacher_forcing and t > 1:
+						topv, topi = outputs[t-1].data.topk(1)
+						
+						input_t = Variable(topi.t())
+						
+						
+					
+					state, hidden, attn_t = self.decoder(input_t, hidden, context, state)
+					
+					state = state.squeeze(0)
+					states.append(state)
+					
+					# from the state, compute the probability distribution
+					output = self.generator(state)
+					
+					outputs.append(output)
+		
+					# compute loss
+					loss_t = self.criterion(output, tgt_label[t])
+					
+					if t == 0:
+						loss = loss_t
+					else:
+						loss = loss + loss_t
+						
+        
+        if not eval and ( mode =='xe' or mode == 'dad' ):
+					loss.div(batch_size).backward()
+				
+        loss_data = loss.data[0]
+				
+        return loss_data, outputs, states
+					
+        
+        
 
-        out, dec_hidden, _attn = self.decoder(tgt, enc_hidden,
-                                              context, init_output)
 
-        return out
+class Generator(nn.Module):
+	
+		
+		def __init__(self, inputSize, dicts):
+			
+			super(Generator, self).__init__()
+			
+			self.inputSize = inputSize
+			self.outputSize = dicts.size()
+			
+			self.net = nn.Sequential(
+        nn.Linear(inputSize, self.outputSize),
+        nn.LogSoftmax())
+
+		def forward(self, input):
+			return self.net(input)
--- onmt/__init__.py
+++ onmt/__init__.py
@@ -5,6 +5,7 @@ from onmt.Dataset import Dataset
 from onmt.Optim import Optim
 from onmt.Dict import Dict
 from onmt.Beam import Beam
+from onmt.gleu import *
 
 # For flake8 compatibility.
 __all__ = [onmt.Constants, onmt.Models, Translator, Dataset, Optim, Dict, Beam]
