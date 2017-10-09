85428fdbdca62819e791a4c5ba5c333be14e3480
Quan Pham <ngoc.pham@kit.edu>
2017-10-09 16:21:21 +0200

    Added new files for utilities, evaluation and metrics

---
+++ rnnlib/__init__.py
@@ -0,0 +1,5 @@
+from rnnlib.Cells import SeparatedBatchNorm1d, LSTMCell
+from rnnlib.RNN import RecurrentLayer
+
+# For flake8 compatibility.
+__all__ = [SeparatedBatchNorm1d, LSTMCell, RecurrentLayer]
